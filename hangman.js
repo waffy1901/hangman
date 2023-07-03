13271ae8e40258a7dfbeec6ffc8f01fcc1874ec9
waffy1901 <waffyahmed@gmail.com>
2023-07-03 18:12:52 -0400

    Sports category of hangman is now almost fully functional

--- hangman.js
+++ hangman.js
@@ -2,6 +2,8 @@
 const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+// array of letters guessed correctly
+var correctlyGuessedLetters = [];
 //number of lives
 var numLives = 6;
 //Current File
@@ -11,25 +13,28 @@ var guessedLetters = [];
 //Current generated word
 var answer;
 //word_bank
-const wordBank = {"Sports": ["Tom Brady", "Celtics", "Lebron James"], 
-"Recondite Knowledge": ["Jennifer Lopez", "Blue Whale", "Ostrich"], 
-"Places": ["Mongolia", "Vatican City", "Machu Picchu"]};
+const wordBank = {"Sports": ["tom brady", "celtics", "lebron james"], 
+"Recondite Knowledge": ["jennifer lopez", "blue whale", "ostrich"], 
+"Places": ["mongolia", "vatican city", "machu picchu"]};
 //word length
 //categories
 const categories = ["Sports", "Recondite Knowledge", "Places"]
 //hint bank
-const hintBank = {"Tom Brady" : "Quarterback that has won the most SuperBowls", 
-"Celtics" : "Franchise that has won the most NBA championships",
-"Lebron James" : "The NBA's all-time leading scorer", 
-"Jennifer Lopez" : "The inspiration behind the creation of Google Images",
-"Blue Whale" : "The animal whose heart weight over 400 pounds",
-"Ostrich" : "This animal has eyes bigger than its brain",
-"Mongolia" : "The world's sparsest population",
-"Vatican City" : "This country is 120 times smaller than Manhattan",
-"Machu Picchu" : "This place very well-preserved and is 75% its original"}
+const hintBank = {"tom brady" : "Quarterback that has won the most SuperBowls", 
+"celtics" : "Franchise that has won the most NBA championships",
+"lebron james" : "The NBA's all-time leading scorer", 
+"jennifer lopez" : "The inspiration behind the creation of Google Images",
+"blue whale" : "The animal whose heart weight over 400 pounds",
+"ostrich" : "This animal has eyes bigger than its brain",
+"mongolia" : "The world's sparsest population",
+"vatican city" : "This country is 120 times smaller than Manhattan",
+"machu picchu" : "This place very well-preserved and is 75% its original"}
 
 //regenerate word button
 function generateWord() {
+    guessedLetters = [];
+    document.getElementById("guessedBox").innerHTML = "Guessed Letters: " + guessedLetters;
+    correctlyGuessedLetters = [];
     if (currentFile === "sports.html") {
         answer = wordBank["Sports"][Math.floor(Math.random() * categories.length)];
     } else if (currentFile === "recondite.html") {
@@ -37,7 +42,12 @@ function generateWord() {
     } else if (currentFile === "places.html") {
         answer = wordBank["Places"][Math.floor(Math.random() * categories.length)];
     }
-    
+    var formattedAnswer = answer.split(" ");
+    var append = "";
+    for (var i = 0; i < formattedAnswer.length; ++i) {
+        append += "_\u00A0".repeat(formattedAnswer[i].length) + "\u00A0\u00A0\u00A0\u00A0";
+    }
+    document.getElementById("setup").innerText = append; 
 }
 
 //hint button
@@ -47,13 +57,41 @@ function getHint() {
 
 //guess
 function guess() {
-    var currentGuess = document.getElementById("letterGuess").value
+    var count = document.getElementById("setup").innerText;
+    var formattedAnswer = answer.split(" ");
+    var currentGuess = document.getElementById("letterGuess").value.toLowerCase();
     if (guessedLetters.includes(currentGuess)) {
         alert("You've already guessed this letter. Try Again!")
     } else {
-        guessedLetters.push(currentGuess)
+        guessedLetters.push(currentGuess);
     }
     document.getElementById('letterGuess').value = '';
+    document.getElementById("guessedBox").innerHTML = "Guessed Letters: " + guessedLetters;
+    for (var i = 0; i < formattedAnswer.length; ++i) {
+        for (var j = 0; j < formattedAnswer[i].length; ++j) {
+            if (formattedAnswer[i][j] === currentGuess) {
+                if (i === 0) {
+                    var firstPart = count.substring(0, j * 2);
+                    var secondPart = count.substring((j * 2) + 1);
+                    count = firstPart + currentGuess + secondPart;
+                } else if (i === 1) {
+                    var firstPart = count.substring(0, (j * 2) + 4 + (formattedAnswer[0].length * 2));
+                    var secondPart = count.substring((j * 2) + 4 + (formattedAnswer[0].length * 2) + 1);
+                    count = firstPart + currentGuess + secondPart;
+                }
+                correctlyGuessedLetters.push(currentGuess);
+            }
+        }
+    }
+    document.getElementById("setup").innerText = count;
+    win();
+}
+
+function win() {
+    var formattedAnswer = answer.split(" ").join("");
+    if (correctlyGuessedLetters.length === formattedAnswer.length) {
+        document.getElementById("winningCard").innerHTML = "Congratulations, you have successfully avoided being hung!";
+    }
 }
 
 function initializePage() {
